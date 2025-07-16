8a88e3acf21a0fc7d62e0285f02191b1ac650223
Lorenzo Carneli <lorenzo.carneli@milkyone.net>
2025-07-16 22:08:03 +0200

    feat: add logger and configuration (#2)

--- cmd/deej/main.go
+++ cmd/deej/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/milkyonehq/deej/pkg/configuration"
+	"github.com/milkyonehq/deej/pkg/logger"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	defer stop()
+
+	logger.Init("info")
+
+	config, err := configuration.New()
+	if err != nil {
+		log.WithError(err).Fatalln("Failed to load configuration.")
+	}
+	log.Infoln("Configuration successfully loaded.")
+
+	logger.Init(config.LogLevel)
+
+	log.Infoln("Bot is starting...")
+
+	log.Infoln("Bot is running. Press CTRL+C to exit.")
+
+	<-ctx.Done()
+
+	log.Infoln("Received shutdown signal. DeeJ bot is shutting down...")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/milkyonehq/deej
 
 go 1.24.4
+
+require (
+	github.com/Netflix/go-env v0.1.2
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
